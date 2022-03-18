07bff115e6f27c63eed8eff661f1124d341a1b74
rosskush <ross.kush@gmail.com>
2022-03-18 15:57:20 -0500

    wel file not writting external

--- load_usg_freyberg.py
+++ load_usg_freyberg.py
@@ -4,9 +4,19 @@ import os
 def main():
     model_ws = os.path.join('model_ws','freyberg.usg')
 
-    mf = flopy.modflow.Modflow.load('freyberg.usg.nam',model_ws=model_ws,version='mfusg',check=True,
-                                    load_only=['disu','bas6'],forgive=False,verbose=True)
+    mf = flopy.mfusg.MfUsg.load('freyberg.usg.nam',model_ws=model_ws,version='mfusg',check=False,
+                                    forgive=True,verbose=False, load_only=['disu','lpf','wel','ghb','rch'])
+
+
+    # convert to all open/close
+    etx_model_ws = os.path.join('model_ws','freyberg.usg.ext')
+    mf.external_path = "."
+    if os.path.exists(etx_model_ws):
+        shutil.rmtree(etx_model_ws)
+    # change dir and write
+    mf.change_model_ws(etx_model_ws, reset_external=True)
+    mf.write_input()
+
 
 if __name__ == '__main__':
     main()
-    
